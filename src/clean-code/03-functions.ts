63da36fd12530d91f7a53bda16f3ffdbad246637
Dario Sandoval Evert <djsandoval@falabella.cl>
2023-07-17 14:44:53 -0400

    feat: nombres de funciones

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,28 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieId( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getAllMovieActors( movieId: string ) {
+        console.log({ movieId });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorById( ActorId: string ) {
+        console.log({ ActorId });
+    }
+    
+    // Crear una película
+    function createMovie(title: string, description: string, rating: number, cast: string[] ) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        console.log('Crear actor', fullName, birthdate);
+        return true;        
+    }
+})();
