9f7c8e64351a09dbe5c684505ec4b667312ac5ed
Dario Sandoval Evert <djsandoval@falabella.cl>
2023-08-11 09:34:22 -0400

    feat: detalles adicionales sobre funciones

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -25,4 +25,10 @@
         console.log('Crear actor', fullName, birthdate);
         return true;        
     }
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
+      if (isDead) return 1500;
+      if (isSeparated) return 2500;
+      return isRetired ? 3000 : 4000;
+    }
 })();
